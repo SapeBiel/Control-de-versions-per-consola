23a8dd9fee996c668c28a49304a715d785a3fabb
AndreiIlerna <toad0064@alumnes.ilerna.com>
2025-11-07 19:37:23 +0100

    Buenas este es un commit de Andrei

--- HolaGit.java
+++ HolaGit.java
@@ -2,6 +2,11 @@ package HolaGit;
 
 public class HolaGit {
 	 public static void main(String[] args) {
+
+			String boss = "Andrei";
+
 	        System.out.println("Hola equipo esto es un control de veriones con consola");
+
+			System.out.println("Este es un mensaje de " + boss);
 	    }
 }
