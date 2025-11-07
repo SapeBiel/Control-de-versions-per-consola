302b9e97fc719a388d6d4fb126bc187064d4ea61
Sape <bielsanchezpey@alumnes.ilerna.com>
2025-11-07 19:29:54 +0100

    Añadido mensaje personal de Biel

--- HolaGit.java
+++ HolaGit.java
@@ -2,6 +2,5 @@ package HolaGit;
 
 public class HolaGit {
 	 public static void main(String[] args) {
-	        System.out.println("Hola equipo esto es un control de veriones con consola");
-	    }
+	        System.out.println("Hola esta es mi parte la de Biel Sape com va Mauri tot be?");	    }
 }
