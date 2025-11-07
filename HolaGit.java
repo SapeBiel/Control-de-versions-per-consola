6cb6d5fce750616c524596016c3c1a6458fc1281
Sape <bielsanchezpey@alumnes.ilerna.com>
2025-11-07 18:58:38 +0100

    Primer commit: archivo HolaGit.java

---
+++ HolaGit.java
@@ -0,0 +1,7 @@
+package HolaGit;
+
+public class HolaGit {
+	 public static void main(String[] args) {
+	        System.out.println("Hola equipo esto es un control de veriones con consola");
+	    }
+}
