a2a9ceaae1a8fa64031da77361e7aced2a42c35e
ElFerraris <galol.exe@alumnes.ilerna.com>
2025-11-10 21:11:16 +0100

    miparte rama-daniel

--- src/HolaGit/HolaGit.java
+++ src/HolaGit/HolaGit.java
@@ -2,6 +2,16 @@ package HolaGit;
 
 public class HolaGit {
 	 public static void main(String[] args) {
+		 
+		   // Mensaje de la rama HEAD
+	        System.out.println("Hola esta es mi parte la de Biel Sape com va Mauri tot be?");
+
+	        // Mensajes de la rama rama-andrei
+	        String boss = "Andrei";
+	        System.out.println("Hola equipo esto es un control de versiones con consola");
+	        System.out.println("Este es un mensaje de " + boss);
+	        
+	        // Rama-Daniel
 	        System.out.println("Ajedrez");
 	        
 	        for (int p1 = 1;p1 <=8; p1++) {
