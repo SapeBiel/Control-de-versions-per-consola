62f834a360c18ad97ad05d73d1aa5af2704de381
ElFerraris <galol.exe@alumnes.ilerna.com>
2025-11-10 20:59:30 +0100

    my rama

--- HolaGit.java
+++
@@ -1,7 +0,0 @@
-package HolaGit;
-
-public class HolaGit {
-	 public static void main(String[] args) {
-	        System.out.println("Hola equipo esto es un control de veriones con consola");
-	    }
-}
---
+++ src/HolaGit/HolaGit.java
@@ -0,0 +1,14 @@
+package HolaGit;
+
+public class HolaGit {
+	 public static void main(String[] args) {
+	        System.out.println("Ajedrez");
+	        
+	        for (int p1 = 1;p1 <=8; p1++) {
+	        	  for (int p2 = 1;p2 <=8; p2++) {
+	  	        	System.out.print("#_");
+	  	        }
+	        	  System.out.print("\r");
+	        }
+	    }
+}
